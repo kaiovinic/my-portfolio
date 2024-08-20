f2903bbddbc65788b989580c85819d52dd0604d1
kaiovinic <kaiovini19@hotmail.com>
2024-08-19 22:52:37 -0300

    Criando meu mobile

---
+++ Components/Nav.tsx
@@ -0,0 +1,30 @@
+import { Bars3Icon } from "@heroicons/react/16/solid";
+import React from "react";
+
+interface Props {
+  openNav: () => void;
+}
+
+const Nav = ({ openNav }: Props) => {
+  return (
+    <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+      <div className="flex justify-between items-center w-[80%] h-[100%] mx-auto">
+        <h1 className=" flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+          WEB
+          <span className="text-yellow-300">DEV</span>
+        </h1>
+        <div className="nav-link">HOME</div>
+        <div className="nav-link">SERVIÇOS</div>
+        <div className="nav-link">SOBRE</div>
+        <div className="nav-link">PROJETOS</div>
+        <div className="nav-link">LINKEDIN</div>
+        <div className="nav-link">CONTATO</div>
+        <div onClick={openNav}>
+          <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
