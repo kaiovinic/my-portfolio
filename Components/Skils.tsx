55936d904fe7bc52722bf42a940c235863d104f2
kaiovinic <kaiovini19@hotmail.com>
2024-08-23 13:57:33 -0300

    Fianalizando seção de skils

--- Components/Skils.tsx
+++ Components/Skils.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SkilsItem from "./SkilsItem";
+import SkilsLanguage from "./SkilsLanguage";
 
 const Skils = () => {
   return (
@@ -9,7 +10,28 @@ const Skils = () => {
       </h1>
       <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center">
         <div>
-          <SkilsItem />
+          <SkilsItem title="React.js Developer" />
+          <SkilsItem title="Next.js Developer" />
+          <SkilsLanguage
+            skill1="HTMl"
+            skill2="CSS"
+            skill3="JavaScript"
+            level1="w-[90%]"
+            level2="w-[80%]"
+            level3="w-[70%]"
+          />
+        </div>
+        <div>
+          <SkilsItem title="TypeScript Developer" />
+          <SkilsItem title="Node.js Developer" />
+          <SkilsLanguage
+            skill1="React.js"
+            skill2="Next.js"
+            skill3="TypeScript"
+            level1="w-[10%]"
+            level2="w-[80%]"
+            level3="w-[50%]"
+          />
         </div>
       </div>
     </div>
--- Components/SkilsItem.tsx
+++ Components/SkilsItem.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const SkilsItem = () => {
-  return <div>SkilsItem</div>;
+interface Props {
+  title: string;
+}
+
+const SkilsItem = ({ title }: Props) => {
+  return (
+    <div className="mb-[4rem] md:mb-[8rem]">
+      <h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">
+        {title}
+      </h1>
+      <p className="text-[#aaaaaa] font-normal w-[80%] text-[18px] opacity-80">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        aperiam repellendus doloribus amet illum quas id provident aliquam
+        deleniti commodi itaque atque ipsa aliquid temporibus odit tenetur
+        tempora, voluptatibus praesentium!
+      </p>
+    </div>
+  );
 };
 
 export default SkilsItem;
