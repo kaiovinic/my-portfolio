7129c250bf36aefc972a9bbd50fefee89ae88a1f
kaiovinic <kaiovini19@hotmail.com>
2024-08-24 23:40:49 -0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/my-portfolio",
+  assetPrefix: "/my-portfolio/",
 };
 
 export default nextConfig;
